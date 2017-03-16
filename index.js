d87c1f84f9a5cdd7be2ef4f9eb9b27451e9c9c6c
Luke Edwards <luke.edwards05@gmail.com>
2017-03-16 09:07:59 -0700

    you can pull defOpts outside the plugin, so that you only have to define it once

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const serve = require('serve')
 const fmt = require('fly/lib/fmt')
 
+const defOpts = {
+  port: 3000,
+  ignore: ['node_modules'],
+  unziped: true,
+  silent: true,
+  verbosity: true
+}
+
 module.exports = {
   name: 'serve',
   every: false,
   files: false,
   * func(globs, opts) {
-    const defOpts = Object.assign({
-      port: 3000,
-      ignore: ['node_modules'],
-      unziped: true,
-      silent: true,
-      verbosity: true
-    }, opts)
+    opts = Object.assign({}, defOpts, opts)
 
     const dir = yield this.$.expand(globs, {mark: true})
 
-    if (dir.length > 1 && defOpts.verbosity) {
+    if (dir.length > 1 && opts.verbosity) {
       this.$.alert(`Only the first found dir will be served`)
     }
 
-    serve(dir[0], defOpts)
+    serve(dir[0], opts)
 
-    if (defOpts.verbosity) {
-      this.$.log(`${fmt.path(dir[0])} is served on ${fmt.path('localhost:')}${fmt.path(defOpts.port)}`)
+    if (opts.verbosity) {
+      this.$.log(`${fmt.path(dir[0])} is served on ${fmt.path('localhost:')}${fmt.path(opts.port)}`)
     }
   }
 }
